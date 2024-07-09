b924486c0852b3d6675faaec3780d10c88bec56c
jrenfroe79 <157231633+jrenfroe79@users.noreply.github.com>
2024-07-08 20:35:27 -0400

    commit updated changes for port 3040

--- script.js/script.js
+++ script.js/script.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv'); // Require the 'dotenv' package
+
+// Load environment variables from .env file in development
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3040; // Set default port to 3040 if PORT is not set
 
 // Serve static files from the 'SDEV_255_Final_Project_Group4' directory
 app.use(express.static(path.join(__dirname, 'SDEV_255_Final_Project_Group4')));
@@ -15,3 +22,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
