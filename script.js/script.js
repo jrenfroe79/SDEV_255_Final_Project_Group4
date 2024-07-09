2c5a53cc25aa0f5f81ea16e5a03f2f514f61a2ff
jrenfroe79 <157231633+jrenfroe79@users.noreply.github.com>
2024-07-08 21:02:10 -0400

    commit changes to script.js

--- script.js/script.js
+++ script.js/script.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv'); // Require the 'dotenv' package
 
+const app = express(); // Create the express app
+
 // Load environment variables from .env file in development
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
-const app = express();
 const PORT = process.env.PORT || 3040; // Set default port to 3040 if PORT is not set
 
 // Serve static files from the 'SDEV_255_Final_Project_Group4' directory
@@ -22,4 +23,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-
