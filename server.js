ceaf4957fdff516b057d7479363b39f6ddfeae9b
jrenfroe79 <157231633+jrenfroe79@users.noreply.github.com>
2024-07-11 17:25:10 -0400

    add public folder

--- server.js
+++ server.js
@@ -1,25 +1,42 @@
+
 const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv'); // Require the 'dotenv' package
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const courseRoutes = require('./routes/courseRoutes');
 
-const app = express(); // Create the express app
 
-// Load environment variables from .env file in development
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
+// connect to mongoDB
+const dbURI = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@sdev255projectteam1.aw1cgbj.mongodb.net/SDEV255ProjectTeam1`;
 
-const PORT = process.env.PORT || 3040; // Set default port to 3040 if PORT is not set
+mongoose.connect(dbURI)
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
-// Serve static files from the 'SDEV_255_Final_Project_Group4' directory
-app.use(express.static(path.join(__dirname, 'sdev-255-final-project-group4')));
+// express app
+const app = express();
 
-// Define a route to serve your HTML file
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+  res.redirect('/courses')
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+//course routes
+app.use('/courses', courseRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
