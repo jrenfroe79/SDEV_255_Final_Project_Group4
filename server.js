886da335c03de9a88139844760f7aa11a86871f2
Josh Fowler <objectiveattitude@gmail.com>
2024-07-11 17:39:27 -0400

    adjust public link structure in appuse method

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // middleware & static files
-app.use(express.static('public'));
+app.use(express.static('/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
